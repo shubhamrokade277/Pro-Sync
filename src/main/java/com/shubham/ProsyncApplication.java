c1e2e1446a3bb9c9df568fdc2fefd2794aa9ca86
Shubham <shubhamrokade277@gmail.com>
2024-06-02 01:06:07 +0530

    First

---
+++ src/main/java/com/shubham/ProsyncApplication.java
@@ -0,0 +1,13 @@
+package com.shubham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsyncApplication.class, args);
+	}
+
+}
